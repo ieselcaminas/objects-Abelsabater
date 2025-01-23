085d19c3fa44b410e7aea53bb0b61ef4120714ef
Abelsabater <abesabmun@alu.edu.gva.es>
2025-01-23 14:27:00 +0100

    Classes y objetos Abel Sabater

--- src/herencia/proyectonetwork/Publicaciones.java
+++ src/herencia/proyectonetwork/Publicaciones.java
@@ -1,11 +1,7 @@
 package herencia.proyectonetwork;
-
-import java.util.ArrayList;
-
 public abstract class Publicaciones {
 private String titulo;
 private String usuario;
-
 public Publicaciones(String titulo, String usuario) { ;
     this.titulo = titulo;
     this.usuario = usuario;
@@ -13,19 +9,15 @@ public Publicaciones(String titulo, String usuario) { ;
 public String getTitulo() {
     return titulo;
 }
-
 public void setTitulo(String titulo) {
     this.titulo = titulo;
 }
-
 public String getUsuario() {
     return usuario;
 }
-
 public void setUsuario(String usuario) {
     this.usuario = usuario;
 }
-
 public String toString(){
     return "Autor"+getUsuario()+"Noticia seleccionada:"+ getTitulo();
 }
