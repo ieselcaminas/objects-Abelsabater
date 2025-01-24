1536af130f3aa8a55b6f466c5cf7dd8d10be50e6
Abelsabater <abesabmun@alu.edu.gva.es>
2025-01-24 13:08:41 +0100

    Classes y objetos Abel Sabater

--- src/herencia/proyectonetwork/Main.java
+++ src/herencia/proyectonetwork/Main.java
@@ -3,14 +3,14 @@ import java.util.ArrayList;
 import java.util.List;
 public class Main {
 public static void main(String[] args) {
-    List<Publicacion> publicacionesList=new ArrayList<>();
+    List<Publicacion> publicaciones=new ArrayList<>();
     Usuario usuario=new Usuario("Pedro","pedrito0957",22);
 PuTextual puTextuales=new PuTextual("2094057M","ferfrfr",usuario,"gscdvdghcdgy");
-publicacionesList.add(puTextuales);
+publicaciones.add(puTextuales);
     usuario=new Usuario("Ana","anita34234",39);
 PuFotografica puFotograficas=new PuFotografica("sdsdo",usuario,"imagen.png");
-    publicacionesList.add(puFotograficas);
-    publicacionesde("Ana",publicacionesList);
+    publicaciones.add(puFotograficas);
+    publicacionesde("Ana",publicaciones);
 }
 private static void publicacionesde(String nombre, List<Publicacion> publicaciones){
     publicaciones.stream()
