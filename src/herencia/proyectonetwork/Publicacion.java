830d5c6a49f47c5b1b9ab09862148a3560d9cd75
Abelsabater <abesabmun@alu.edu.gva.es>
2025-01-28 14:19:31 +0100

    Classes y objetos Abel Sabater

--- src/herencia/proyectonetwork/Publicacion.java
+++ src/herencia/proyectonetwork/Publicacion.java
@@ -15,39 +15,30 @@ public Publicacion(String titulo, Usuario usuario) {
     this.megusta = 0;
     this.fecha= new Date();
 }
-
 public Date getFecha() {
     return fecha;
 }
-
 public void setFecha(Date fecha) {
     this.fecha = fecha;
 }
-
 public int getMegusta() {
     return megusta;
 }
-
 public void setMegusta() {
     this.megusta++;
 }
-
 public String getTitulo() {
     return titulo;
 }
-
 public void setTitulo(String titulo) {
     this.titulo = titulo;
 }
-
 public Usuario getUsuario() {
     return usuario;
 }
-
 public void setUsuario(Usuario usuario) {
     this.usuario = usuario;
 }
-
 public String toString() {
     return "Autor" + getUsuario() + "Noticia seleccionada:" + getTitulo()+" fecha publicado" +getFecha();
 }
