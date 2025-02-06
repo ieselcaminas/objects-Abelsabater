3e29798b149a514779abf31ce1c9421d9ae21dd1
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-06 14:04:56 +0100

    Classes y objetos Abel Sabater

---
+++ src/POO/ejercicio7/Lector.java
@@ -0,0 +1,33 @@
+package POO.ejercicio7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lector {
+private String dni;
+private String nombre;
+List<Exemplar>exemplars;
+
+public Lector(String dni, String nombre) {
+    this.dni = dni;
+    this.nombre = nombre;
+    this.exemplars=new ArrayList<>();
+}
+public void add(Exemplar exemplar) {
+    this.exemplars.add(exemplar);
+}
+public String getDni() {
+    return dni;
+}
+
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+public List<Exemplar> getExemplars() {
+    return exemplars;
+}
+}
