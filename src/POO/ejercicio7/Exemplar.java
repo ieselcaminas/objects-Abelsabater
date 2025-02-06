43a541b9956f89cb976487a7be666f3553395613
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-06 14:30:33 +0100

    Classes y objetos Abel Sabater

--- src/POO/ejercicio7/Exemplar.java
+++ src/POO/ejercicio7/Exemplar.java
@@ -1,16 +1,27 @@
 package POO.ejercicio7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exemplar {
 private int n_reg;
 private String nombre;
 private LLibre lLibre;
+private List<Lector>lectors;
 
 public Exemplar(int n_reg, String nombre, LLibre lLibre) {
     this.n_reg = n_reg;
     this.nombre = nombre;
     this.lLibre = lLibre;
+    this.lectors=new ArrayList<>();
 }
 
+public List<Lector> getLectors() {
+    return lectors;
+}
+public void add(Lector lector) {
+    this.lectors.add(lector);
+}
 public int getN_reg() {
     return n_reg;
 }
--- src/POO/ejercicio7/Main.java
+++ src/POO/ejercicio7/Main.java
@@ -16,11 +16,15 @@ public static void main(String[] args) {
     Editorial hols=new Editorial(2,"Hols");
     editorials.add(hols);
         Autor ximoPuig=new Autor(1,"XimoPuig");
-        LLibre lLibre=new LLibre("Los 3 cerditos",1,hols,terror);
-        lLibre.add(ximoPuig);
-        ximoPuig.add(lLibre);
-        terror.add(lLibre);
-        hols.add(lLibre);
-
+        LLibre cerdos=new LLibre("Los 3 cerditos",1,hols,terror);
+        cerdos.add(ximoPuig);
+        ximoPuig.add(cerdos);
+        terror.add(cerdos);
+        hols.add(cerdos);
+            Exemplar excer=new Exemplar(1,"Exemplar1",cerdos);
+            cerdos.add(excer);
+            Lector lector1=new Lector("20970899M","Abel");
+            excer.add(lector1);
+            lector1.add(excer);
 }
 }
