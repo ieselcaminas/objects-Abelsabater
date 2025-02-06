c001c35557160d56f88322dcbe3c801f8c1e2d22
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-06 14:23:56 +0100

    Classes y objetos Abel Sabater

--- src/POO/ejercicio7/Editorial.java
+++ src/POO/ejercicio7/Editorial.java
@@ -1,14 +1,25 @@
 package POO.ejercicio7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Editorial {
 private int id;
 private String nombre;
+private List<LLibre> lLibres;
 
 public Editorial(int id, String nombre) {
     this.id = id;
     this.nombre = nombre;
+    this.lLibres=new ArrayList<>();
 }
 
+public List<LLibre> getlLibres() {
+    return lLibres;
+}
+public void add(LLibre lLibre) {
+    this.lLibres.add(lLibre);
+}
 public int getId() {
     return id;
 }
--- src/POO/ejercicio7/Main.java
+++ src/POO/ejercicio7/Main.java
@@ -5,10 +5,17 @@ import java.util.List;
 
 public class Main {
 public static void main(String[] args) {
+    List<Editorial>editorials=new ArrayList<>();
     List<Tema>temas=new ArrayList<>();
     Tema terror=new Tema(1,"Terror");
     Tema historia=new Tema(2,"Historia");
     temas.add(terror);
     temas.add(historia);
+    Editorial edebe=new Editorial(1,"Edebe");
+    editorials.add(edebe);
+    Editorial hols=new Editorial(2,"Hols");
+    editorials.add(hols);
+    LLibre lLibre=new LLibre("Los 3 cerditos",1,hols,terror);
+
 }
 }
