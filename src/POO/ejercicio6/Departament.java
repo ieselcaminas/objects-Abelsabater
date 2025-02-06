d9e9469fbed9de2c05d0ab3ff16de457da8a732a
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-06 13:43:12 +0100

    Classes y objetos Abel Sabater

--- src/POO/ejercicio6/Departament.java
+++ src/POO/ejercicio6/Departament.java
@@ -8,11 +8,15 @@ private int id;
 private String nombre;
 private AreaConeixement areaConeixement;
 private List<Professor> professores;
+private List<Catedra> catedras;
 public Departament(int id, String nombre, AreaConeixement areaConeixement) {
     this.id = id;
     this.nombre = nombre;
     this.areaConeixement = areaConeixement;
     this.professores=new ArrayList<>();
+}
+public void add(Catedra catedra){
+    this.catedras.add(catedra);
 }
 public void add(Professor professor){
     this.professores.add(professor);
--- src/POO/ejercicio6/Professor.java
+++ src/POO/ejercicio6/Professor.java
@@ -1,16 +1,23 @@
 package POO.ejercicio6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Professor {
 private String nom;
 private int id;
 private Departament departament;
+private List<Catedra> catedras;
 
 public Professor(String nom, int id, Departament departament) {
     this.nom = nom;
     this.id = id;
     this.departament = departament;
+    this.catedras=new ArrayList<>();
 }
-
+public void add(Catedra catedra){
+    this.catedras.add(catedra);
+}
 public int getId() {
     return id;
 }
