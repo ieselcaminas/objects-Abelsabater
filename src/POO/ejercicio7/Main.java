7c693f2464d9f0318185fd886ddc424009c1a2ae
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-06 14:11:55 +0100

    Classes y objetos Abel Sabater

---
+++ src/POO/ejercicio7/Main.java
@@ -0,0 +1,14 @@
+package POO.ejercicio7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+public static void main(String[] args) {
+    List<Tema>temas=new ArrayList<>();
+    Tema terror=new Tema(1,"Terror");
+    Tema historia=new Tema(2,"Historia");
+    temas.add(terror);
+    temas.add(historia);
+}
+}
