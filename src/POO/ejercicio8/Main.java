cdf96f7b3f359b6611b7bfb936ff457bc8de700b
abesabmun <abesabmun@alu.edu.gva.es>
2025-02-11 12:47:20 +0100

    Classes y objetos Abel Sabater

--- src/POO/ejercicio8/Main.java
+++ src/POO/ejercicio8/Main.java
@@ -9,15 +9,20 @@ public class Main {
         Subasta subasta=new Subasta(1);
 
 
-        Lote lote=new Lote(1);
+        Lote lote1=new Lote(1);
 
         Articulo articulo1=new Articulo("Patin",12);
 
-        lote.adda(articulo1);
+        lote1.adda(articulo1);
         Articulo articulo2=new Articulo("Bicicleta",79);
-        lote.adda(articulo2);
+        lote1.adda(articulo2);
 
-        subasta.addL(lote);
+        Puja puja1=new Puja(101,1,lote1);
+        lote1.addp(puja1);
+        Puja puja2=new Puja(110,1,lote1);
+        lote1.addp(puja2);
+
+        subasta.addL(lote1);
         subastas.add(subasta);
     }
 }
