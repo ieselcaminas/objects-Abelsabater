94e4d5e03609ad4fe9e9701a4cb06bf5c85e93a8
abesabmun <abesabmun@alu.edu.gva.es>
2025-02-11 12:44:01 +0100

    Classes y objetos Abel Sabater

--- src/POO/ejercicio8/Lote.java
+++ src/POO/ejercicio8/Lote.java
@@ -7,11 +7,14 @@ public class Lote {
     private int cod;
     private double precio;
     private List<Articulo>articulos;
+    private List<Puja>pujas;
 
     public Lote(int cod) {
         this.cod = cod;
         this.precio=0;
         this.articulos=new ArrayList<>();
+        this.pujas=new ArrayList<>();
+
     }
 
     public int getCod() {
@@ -38,5 +41,8 @@ public class Lote {
 
     public void adda(Articulo articulo){
         this.articulos.add(articulo);
+    }
+    public void addp(Puja puja){
+        this.pujas.add(puja);
     }
 }
