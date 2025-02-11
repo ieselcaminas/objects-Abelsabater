b4ffaa256aac1d5d8a9b1990b83e760be13d8693
abesabmun <abesabmun@alu.edu.gva.es>
2025-02-11 12:42:18 +0100

    Classes y objetos Abel Sabater

--- src/POO/ejercicio8/Puja.java
+++ src/POO/ejercicio8/Puja.java
@@ -1,4 +1,33 @@
 package POO.ejercicio8;
 
 public class Puja {
+    private int cod;
+    private double cantidad;
+    private Lote lote;
+
+    public Puja(double cantidad, int cod, Lote lote) {
+        this.cantidad = cantidad;
+        this.cod = cod;
+        this.lote = lote;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Lote getLote() {
+        return lote;
+    }
+
+    public void setLote(Lote lote) {
+        this.lote = lote;
+    }
 }
