82e15dacccc0cb5a3f6e0598b94f8aef495def93
Abelsabater <abesabmun@alu.edu.gva.es>
2025-02-13 13:32:09 +0100

    Classes y objetos Abel Sabater

--- src/POO/Maquinaexpendedora/Cliente.java
+++ src/POO/Maquinaexpendedora/Cliente.java
@@ -1,13 +1,26 @@
 package POO.Maquinaexpendedora;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cliente {
     private double cantidad;
-
+    private List<Tiquet> tiquets;
 
     public Cliente(double cantidad) {
         this.cantidad = cantidad;
+        this.tiquets=new ArrayList<>();
     }
 
+    public List<Tiquet> getTiquets() {
+        return tiquets;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
 }
